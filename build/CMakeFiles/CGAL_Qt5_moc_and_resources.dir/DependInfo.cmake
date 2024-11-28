343a800feef1a905f9a2e003c4597dfe18860a9c
t225 <thch116@gmail.com>
2024-11-28 13:53:35 +0200

    version 2

---
+++ build/CMakeFiles/CGAL_Qt5_moc_and_resources.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/t225/project_2/build/CGAL_Qt5_moc_and_resources_autogen/mocs_compilation.cpp" "/home/t225/project_2/build/CMakeFiles/CGAL_Qt5_moc_and_resources.dir/CGAL_Qt5_moc_and_resources_autogen/mocs_compilation.cpp.o"
+  "/home/t225/project_2/build/qrc_CGAL.cpp" "/home/t225/project_2/build/CMakeFiles/CGAL_Qt5_moc_and_resources.dir/qrc_CGAL.cpp.o"
+  "/home/t225/project_2/build/qrc_File.cpp" "/home/t225/project_2/build/CMakeFiles/CGAL_Qt5_moc_and_resources.dir/qrc_File.cpp.o"
+  "/home/t225/project_2/build/qrc_Input.cpp" "/home/t225/project_2/build/CMakeFiles/CGAL_Qt5_moc_and_resources.dir/qrc_Input.cpp.o"
+  "/home/t225/project_2/build/qrc_Triangulation_2.cpp" "/home/t225/project_2/build/CMakeFiles/CGAL_Qt5_moc_and_resources.dir/qrc_Triangulation_2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "CGAL_USE_BASIC_VIEWER"
+  "CGAL_USE_GMPXX=1"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "CGAL_Qt5_moc_and_resources_autogen/include"
+  "."
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/triangulation.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/t225/project_2/src/main.cpp" "/home/t225/project_2/build/CMakeFiles/triangulation.dir/src/main.cpp.o"
+  "/home/t225/project_2/src/triangulation.cpp" "/home/t225/project_2/build/CMakeFiles/triangulation.dir/src/triangulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "CGAL_USE_BASIC_VIEWER"
+  "CGAL_USE_GMPXX=1"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "."
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/t225/project_2/build/CMakeFiles/CGAL_Qt5_moc_and_resources.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
